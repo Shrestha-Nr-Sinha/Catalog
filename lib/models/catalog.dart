851fb838b1cf2f40559949acbba867e65fdf437a
Shrestha Narayan Sinha <shrestha.narayansinha04@gmail.com>
2023-06-12 16:32:39 +0530

    Day 11 Completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,29 @@
+//import 'package:flutter/material.dart';
+
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
+
+final products = [
+  Item(
+    id: "",
+    name: "",
+    desc: "",
+    price: 999,
+    color: "",
+    image: "",
+  )
+];
